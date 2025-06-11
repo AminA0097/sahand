645b966ad68e786b48e5d37f83f041a8cf73ba93
AminA0097 <springmcserv@gmailc.om>
2025-06-11 16:33:10 +0330

    init

---
+++ src/main/java/com/userservice/sahand/SahandApplication.java
@@ -0,0 +1,13 @@
+package com.userservice.sahand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SahandApplication.class, args);
+    }
+
+}
